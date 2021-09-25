db5d8f9610f33fcb57c498ad3beb2a7e971fcf13
pigfai <1713954081@qq.com>
2021-09-25 16:04:21 +0800

    202109251604_first_commit

---
+++ 09-3-pic_dataset.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from d2l import torch as d2l
+from torch.utils import data
+from torchvision.datasets.mnist import read_image_file, read_label_file
+from torchvision.datasets.utils import extract_archive
+
+
+batch_size = 256
+
+
+def get_dataloader_workers():  #@save
+    """使用4个进程来读取数据。"""
+    return 0
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中。"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="D://d2l-data/", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="D://d2l-data/", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
+for X, y in train_iter:
+    print(X.shape, X.dtype, y.shape, y.dtype)
+    break
